511bf4b112186da2dea4a759c34544c1f1a56607
Oleg Yurchenko <oleg@bgmot.com>
2022-11-08 20:14:15 -0800

    Added a flag functionality where you can put -f before the arguments to output formatted string (passed through xxd) or -h for just the hexcode. This is optional and if no flag is passed then the program runs as normal, and outputs both. This is potentionally useful for piping instead of copy+pasting

--- stacksmashtool.py
+++ stacksmashtool.py
@@ -6,9 +6,12 @@ addr = ""
 num = 0
 
 if (len(sys.argv) > 1):
-    beginstr = sys.argv[1]
-    addr = sys.argv[2]
-    num = int(sys.argv[3])
+    op = 0
+    if(len(sys.argv) == 5):
+        op += 1
+    beginstr = sys.argv[1 + op]
+    addr = sys.argv[2 + op]
+    num = int(sys.argv[3 + op])
 else:
     print("Enter the code you want to run:")
     beginstr = str(input())
@@ -21,6 +24,12 @@ outstr = beginstr
 for i in range(num):
     outstr = outstr + addr
 
-print("Your hex string:\n" + outstr) 
-print("Your formatted string:")
-os.system("echo " + outstr + " | ""xxd -r -p ")
+if (len(sys.argv) == 5):
+    if(sys.argv[1] == "-h"):
+        print(outstr)
+    elif(sys.argv[1] == "-f"):
+        os.system("echo " + outstr + " | xxd -r -p ")
+else:
+    print("Your hex string:\n" + outstr) 
+    print("Your formatted string:")
+    os.system("echo " + outstr + " | xxd -r -p ")
